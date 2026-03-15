5c6804d7a5b857554b0816dd6c2ffd1555b9adeb
Praymore Omeni <omenipraymore18@gmail.com>
2026-03-15 19:13:10 +0000

    Complete temple page

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+
+navigation.classList.toggle("open");
+
+menuButton.textContent =
+navigation.classList.contains("open") ? "✖" : "☰";
+
+});
+
+document.querySelector("#year").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+"Last Modified: " + document.lastModified;
